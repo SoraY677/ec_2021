5c9f9c2c70e6495f7d86e0a109475b31b0fcd85c
SoraY677 <SoraY677@outlook.com>
2021-11-04 22:26:09 +0900

    cythonをいったん削除

---
+++ src/process.py
@@ -0,0 +1,25 @@
+'''
+処理のブロックごとに関数で分けたスクリプト
+'''
+
+import cython
+
+# 最適解の生成
+
+
+def create_sol():
+    pass
+
+# 最適解を計算する
+
+
+def search_sol():
+    pass
+    # return (おそらく)
+
+# 解の提出
+
+
+def submit():
+    pass
+    # return (おそらく)
---
+++ src/util.py
@@ -0,0 +1,20 @@
+'''
+便利な関数群
+'''
+
+import cython
+
+# 解の類似度を求める
+
+
+def ans_hist():
+    # DICE係数を用いる
+    # https://mieruca-ai.com/ai/jaccard_dice_simpson/
+    # *各要素のDICE係数を掛け合わせ総合類似度を計算
+    pass
+
+# 解を整形し、提出できる文字列にする
+
+
+def ans_format():
+    pass
