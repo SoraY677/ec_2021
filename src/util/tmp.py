32d7c2ef124e9b9a9196ae50673593728bbff5ed
SoraY677 <SoraY677@outlook.com>
2021-11-06 09:17:15 +0900

    とりあえず仮置き

--- src/util/tmp.py
+++ src/util/tmp.py
@@ -6,14 +6,24 @@
 # 解の類似度を求める
 
 
-def ans_hist():
+def hist_2sol(sol_1, sol_2):
+    '''
+    二つの解の類似度を各項目ごとに算出
+
+    Args:
+    sol_1 (dict): 解1
+    sol_2 (dict): 解2
+    '''
+
     # DICE係数を用いる
     # https://mieruca-ai.com/ai/jaccard_dice_simpson/
     # *各要素のDICE係数を掛け合わせ総合類似度を計算
-    pass
+
+    return {}
+
 
 # 解を整形し、提出できる文字列にする
 
 
-def ans_format():
+def sol_format():
     pass
