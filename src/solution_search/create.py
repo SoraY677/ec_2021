0b8bbc56e9dfee9dac4e1a2f3e2137f1ff560469
SoraY677 <SoraY677@outlook.com>
2021-11-07 22:12:55 +0900

    類似度を求める処理は探索途中で使う

--- src/solution_search/create.py
+++ src/solution_search/create.py
@@ -53,18 +53,4 @@ def create_init_sol():
     return result
 
 
-def hist_2sol(sol_1, sol_2):
-    '''
-    二つの解の類似度を各項目ごとに算出
-
-    Args:
-    sol_1 (dict): 解1
-    sol_2 (dict): 解2
-    '''
-    
-    # DICE係数を用いる
-    # https://mieruca-ai.com/ai/jaccard_dice_simpson/
-    # *各要素のDICE係数を掛け合わせ総合類似度を計算
-
-    return 1
 
