3664a95cefde2fb91d60630b7659bbf6cfdcc7b8
SoraY677 <SoraY677@outlook.com>
2021-11-17 20:20:14 +0900

    解生成にオプション情報を付加

--- src/solution_search/create.py
+++ src/solution_search/create.py
@@ -5,26 +5,44 @@
 import sys
 import common
 from copy import copy 
-from random import shuffle, randint
+from random import seed, shuffle, randint
 
 sys.path.append('../')
 from util import const
 from util import log
 
 
-def create_init_sol():
+def create_init_sol(function_id=None, city=None, seeds=None):
     '''
-    解を生成する
+    初期の解を生成する
+
+    Args:
+        function_id (str, optional): 評価関数の指定番号 ex)"[1,2]". Defaults to None.
+        city (str, optional): 年の名称 ex)"hakodate" . Defaults to None.
+        seeds (str, optional): シード値 ex)"[123,42,256]". Defaults to None.
+
+    Returns:
+        dist: 生成した解
     '''
     result = {}
+    if  function_id is None or \
+        city is None or\
+        seeds is None:
+        log.error('引数の指定が足りません！')
 
     # 給付金額を決定
     result[const.PAYMENT_NAME] = 5 
 
+    # オプション決め
+    result[const.FUNCTION_ID] = function_id
+    result[const.CITY] = city
+    result[const.SEEDS] = seeds
+
+
     # 各属性に対してランダムで属性を取得するという処理
     attr_i = 1
     for attr_key in const.ALL_ATTRIBUTE_DICT:
-        log.debug('A'+str(attr_i))
+        log.debug('A'+ str(attr_i))
         target = []  # 対象として決定した配列を入れておく場所
         # 対象属性の配列をシャッフル
         copy_attr_array = list(const.ALL_ATTRIBUTE_DICT[attr_key])
@@ -51,6 +69,4 @@ def create_init_sol():
         log.debug('==========================')
 
     return result
-
-
 
