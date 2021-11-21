22027b28f24213df087fc933990a862982ba7fb8
SoraY677 <SoraY677@outlook.com>
2021-11-21 09:26:02 +0900

    メイン関数に記載

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from src import submit
-from src.solution_search import create
+import time
+from src.util import const 
+from src.util import log
+
+from src import process
 
 
-sol = create.create_init_sol(function_id="[1,2]",city="hakodate",seeds="[123,42,256]")
-submit.regist(sol, ["python","test/windows/syn_pop.py"] )
-result = submit.run()
+const.SEARCH_MAX = 3 # 解探索限界で定義
+const.POPLATION_MAX = 1 # 扱う解集合の数を定義
+const.FUNCTION_ID = "[1,2]"
+const.CITY_ID = "hakodate"
+const.SEEDS_ID = "[123,42,256]"
 
-print(result)
+
+
+if __name__ == "__main__":
+
+	# タイマー計測開始
+	start_time = time.time()
+
+	# 解の生成
+	process.create_sol(const.POPLATION_MAX, const.FUNCTION_ID, const.CITY_ID, const.SEEDS_ID)
+	loop_num = const.SEARCH_MAX / const.POPLATION_MAX
+
+	for _ in range(int(loop_num)):
+		# 解の評価
+		process.evaluate_sol(base_command=["python","test/windows/syn_pop.py"])
+
+	# 	# 最良解の探索
+	# 	pass
+
+	# タイマー計測終了
+	end_time = time.time()
+
+	log.info('経過時間:' + str(end_time - start_time) )
+
+
 
 
 
