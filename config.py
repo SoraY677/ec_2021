3c2491ee56c061887868747009075d7b3e0a1845
SoraY677 <SoraY677@outlook.com>
2021-12-05 15:53:32 +0900

    ベースコマンドを追加

--- config.py
+++ config.py
@@ -8,6 +8,12 @@ from src.util import const
 # テストモードの真偽
 const.IS_TEST = True 
 
+# ベースコマンド
+if const.IS_TEST:
+	const.BASE_COMMAND = ["python","test/windows/syn_pop.py"]
+else:
+	const.BASE_COMMAND = []
+
 # 解提出時の各パラメータ
 const.FUNCTION_ID = "[1]"
 const.CITY_ID = "hakodate"
@@ -20,3 +26,4 @@ const.ANALYZE_OPTIMAZE_SOLUTION = 'analyze/OptimalSolution-graph'
 const.SEARCH_MAX = 100 # 解探索数の限界
 const.POPLATION_MAX = 2 # 解集団数
 const.EVOLVE_THRESHOLD = 0.05 # 二つの解の類似度を用いた進化をする際に使用する閾値
+const.MULATION_THRESHOLD = 0.8 # 突然変異する閾値
