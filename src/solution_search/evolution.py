55f53c595070766e00de33acc5a474fc1b7a89ed
SoraY677 <SoraY677@outlook.com>
2021-12-05 15:20:23 +0900

    まとめられる関数をひとまとめに

--- src/solution_search/evolution.py
+++ src/solution_search/evolution.py
@@ -5,17 +5,15 @@
 from random import random, randint, shuffle
 from copy import copy
 
-from types import new_class
 from ..util import const
 from ..util import log
 
 from . import create
 from . import common
 
-const.EVOLVE_THRESHOLD = 0.05
+const.HIST_BASE_PRICE = 20 # FIXME 類似度算出用の規定値
 
-
-def get_attr_noinclude_list(cur_list, attr_key):
+def append_one_attr_noinclude(cur_list, attr_key):
     '''
     まだ含まれていない要素のリストを取得
 
@@ -23,14 +21,19 @@ def get_attr_noinclude_list(cur_list, attr_key):
     - cur_list (list) : 現在の要素
     - attr_key (str): 属性のキー名
     '''
-
-    diff = set(cur_list) ^  set(const.ALL_ATTRIBUTE_DICT[attr_key])
-    return list(diff)
-
+    append_list = copy(cur_list)
+    # 不足分を知らべる
+    diff = list(set(append_list) ^ set(const.ALL_ATTRIBUTE_DICT[attr_key]))
+    # あればそこからランダムに一つ加えた配列を返す
+    if len(diff) > 0:
+        append_list.append(diff[randint(0,len(append_list)-1)])
+    # 無ければ元の配列をそのまま返す
+    return cur_list
 
 def challenge_evolve_prudent(sol, feasible):
     '''
     解に少し変更を加えて新たな解の生成を試みる
+    (消極派の解専用)
 
     Args:
     - sol [dict] : 現在の解情報
@@ -38,54 +41,52 @@ def challenge_evolve_prudent(sol, feasible):
     '''
     new_sol = copy(sol)
 
-    # 解の評価結果が制約条件を満たしていない時
+    # 解の評価結果が制約条件を満たせていない時
+    # = 要素数が多すぎ and(or) 給付金額が高すぎ
     if feasible == False:
-        change_attr_max = randint(1,3)
-        shuffle_list = copy(list(const.ATTRIBUTE_KEY_LIST))
+        # 変更対象とする種別を決定
+        change_attr_max = randint(1,3) 
+        shuffle_list = copy(list(const.ATTRIBUTE_KEY_LIST))  
         shuffle(shuffle_list)
         shuffle_list = shuffle_list[0:change_attr_max-1]
+        # 要素数をランダムに-1
         for key in shuffle_list:
             if len(new_sol[key]) > 0 :
                 new_sol[key].pop(randint(0, len(new_sol[key])-1))
-                new_sol[key].pop(randint(0, len(sol[key])-1))
-                lack_arr = common.get_need_attr(new_sol[key],const.ATTRIBUTE_KEY_LIST.index(key) + 1).copy()
-                new_sol[key].extend(lack_arr)
-                new_sol[key].sort()
+                new_sol[key] = common.get_complete_attr(new_sol[key], key) # 解が不完全の可能性があるため
+
         # 金額の変化
-        if randint(0,100) < [80,50,30][3 - change_attr_max]:
+        # 変更対象となった種別数に応じて金額の変更確率を変化させる
+        if randint(0,100) < [80,50,30][3 - change_attr_max]: 
             new_sol[const.PAYMENT_KEY] /= random() + 1
-            new_sol[const.PAYMENT_KEY] = round(new_sol[const.PAYMENT_KEY], 5)
     
-    # 解の評価結果が制約条件を満たしている
+    # 解の評価結果が制約条件を満たしている時
     else :
         change_attr_max = randint(1,5)
         for _ in range(change_attr_max):
             target_key = const.ATTRIBUTE_KEY_LIST[randint(0,len(const.ATTRIBUTE_KEY_LIST) - 1)]
             # 50%で要素増加
             if randint(0, 100) > 50:
-                noinclude_list = get_attr_noinclude_list(new_sol[target_key], target_key)
-                
-                if len(noinclude_list) > 0:
-                    new_sol[target_key].append(noinclude_list[randint(0,len(noinclude_list)-1)])
+                # 含まれていない要素があれば追加
+                new_sol[target_key] = append_one_attr_noinclude(new_sol[target_key], target_key)
+
             # 残り50%で要素削減
             else:
-                new_sol[target_key].pop(randint(0, len(sol[target_key])-1))
-                lack_arr = common.get_need_attr(new_sol[target_key], const.ATTRIBUTE_KEY_LIST.index(target_key) + 1).copy()
-                new_sol[target_key].extend(lack_arr)
-            
-            new_sol[target_key].sort()
+                new_sol[target_key].pop(randint(0, len(new_sol[target_key])-1))
+                new_sol[target_key] = common.get_complete_attr(new_sol[target_key], target_key) # 不足する要素をなしにする
 
-        # 金額の変化        
+        # 金額の変化
         new_sol[const.PAYMENT_KEY] /= random() * 2 + 0.000001
-        new_sol[const.PAYMENT_KEY] = round(new_sol[const.PAYMENT_KEY], 5)
 
+    new_sol[const.PAYMENT_KEY] = round(new_sol[const.PAYMENT_KEY], 5) # 少数を適当なところで切る
 
     log.info('new_sol:' + str(new_sol))
     return new_sol
 
 def challenge_evolve_agressive(sol):
     '''
-    解を大幅に変化
+    解を大幅に変化させる
+    (積極派の解専用)
 
     Args:
     - sol (dict): 解情報
@@ -93,39 +94,37 @@ def challenge_evolve_agressive(sol):
     Returns:
     - dict: 新規解情報
     '''
-
-    new_sol = copy(sol)
+    
     # 完全ランダム再生成
     if random() > 0.8:
         new_sol = create.create_init_sol(const.FUNCTION_ID, const.CITY_ID, const.SEEDS_ID)
 
     else:
-        change_attr_max = randint(1,15)
+        new_sol = copy(sol)
+
+        change_attr_max = randint(1,15) # 変更する回数
         for _ in range(change_attr_max):
             target_key = const.ATTRIBUTE_KEY_LIST[randint(0, len(const.ATTRIBUTE_KEY_LIST) - 1)]
+
             # 50%で要素を追加
             if randint(0, 100) > 50:
-                noinclude_list = get_attr_noinclude_list(new_sol[target_key], target_key)
-                if len(noinclude_list) > 0:
-                    new_sol[target_key].append(noinclude_list[randint(0,len(noinclude_list)-1)])
+                new_sol[target_key] = append_one_attr_noinclude(new_sol[target_key], target_key)
+
             # 残り50%で要素削減
             else:
                 if len(sol[target_key]) > 0 :
                     new_sol[target_key].pop(randint(0, len(sol[target_key])-1))
 
-            # 不足分を補う
-            lack_arr = common.get_need_attr(new_sol[target_key], const.ATTRIBUTE_KEY_LIST.index(target_key) + 1).copy()
-            new_sol[target_key].extend(lack_arr)
-            new_sol[target_key].sort()
-
+        # 不足分を補う
+        for key in const.ATTRIBUTE_KEY_LIST:
+            new_sol[key] = common.get_complete_attr(new_sol[key], target_key) 
     
+        # 金額を変更
         for _ in range(5):
             new_sol[const.PAYMENT_KEY] /= random() * 2 + 0.000001
             new_sol[const.PAYMENT_KEY] = round(new_sol[const.PAYMENT_KEY], 5)
 
     return new_sol
-
-
 
 def tracking_evolve(prudent_sol, prudent_eval, aggresive_sol,aggresive_eval):
     '''
@@ -157,7 +156,6 @@ def tracking_evolve(prudent_sol, prudent_eval, aggresive_sol,aggresive_eval):
 
     return False
 
-
 def hist_2num(number_a, number_b):
     '''
     二つの値の類似度を算出する
@@ -167,8 +165,7 @@ def hist_2num(number_a, number_b):
         number_b (int): 比較先数値
     '''
 
-    return (20 - abs(number_a - number_b)) / 20
-
+    return (const.HIST_BASE_PRICE - abs(number_a - number_b)) / const.HIST_BASE_PRICE 
 
 def hist_2array(list_a, list_b):
     '''
