c18b70bcaa7aea7f26c8e1515d2b7adac01ebb0f
SoraY677 <SoraY677@outlook.com>
2021-12-05 16:17:11 +0900

    不要な引数を削除するように変更

--- src/process.py
+++ src/process.py
@@ -1,54 +1,37 @@
 '''
 処理のブロックごとに関数で分けたスクリプト
 '''
+
+
+from . import submit 
 from .util import log
 from .solution_search import population
-from . import submit 
 
-def create_sol(sol_max, function_id=None, city=None, seeds=None):
+def create():
     '''
     解を生成
-
-    Args:
-    - sol_max (int): 解の生成数
     '''
+    population.create()
 
-    ### エラーハンドリング
-    if sol_max <= 0:
-        log.error("create_solの引数はsol_maxは「> 0」でなければならない。")
-    elif function_id is None or \
-         city is None or\
-         seeds is None:
-        log.error('create_solにおける引数の指定が足りません！')
-
-    population.create_sol(sol_max, function_id, city, seeds)
-
-def evaluate_sol(base_command = []):
+def evaluate():
     '''
     解の評価
 
     Args:
     - base_command (list[str]): 基本コマンド
     '''
-    ### エラーハンドリング
-    # リストであること
-    if not isinstance(base_command, list):
-        log.error("「base_command = list型」である必要があります。")
-    # コマンドの指定があること
-    elif len(base_command) == 0:
-        log.error("base_commandに何も入っていません")
 
     # 解集団をまとめて評価するためのラッパー関数
     def submit_pop(sol_pop):
         for sol in sol_pop:
-            submit.regist(sol, base_command)
+            submit.regist(sol)
         return submit.run()
 
 
-    population.evaluate_sol(submit_pop)
+    population.evaluate(submit_pop)
 
-def evolove_sol( ):
+def evolove( ):
     '''
     解を進化させる
     '''
-    population.evolve_sol()
+    population.evolve()
