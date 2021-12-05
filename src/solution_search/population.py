c9f81d79dbdc2e84d0ed2eb8a13e5eb0ae07943f
SoraY677 <SoraY677@outlook.com>
2021-12-05 16:16:26 +0900

    名前変更と処理の改善

--- src/solution_search/population.py
+++ src/solution_search/population.py
@@ -1,13 +1,13 @@
 '''
+解全体の管理を行うスクリプト
 - 解探索に用いられる変数全体の保持
 - 解探索中の各処理をまとめる
 '''
-from . import create
-from . import evolution
 
 from random import randint
 
-
+from . import creater
+from . import evolution
 from ..util import const
 from ..util import log
 from ..util import graph
@@ -15,29 +15,22 @@ from ..util import graph
 const.SEARCH_STYLE_PRUDENT = 0 # 探索のスタイルが慎重派 (小さく変化)
 const.SEARCH_STYLE_AGGRESSIVE = 1 # 探索のスタイルが積極派 (大きく変化)
 
-const.SEARCH_DIRECTION_PLUS = 1 # 探索の際に属性や金額を増やす方向で動く
-const.SEARCH_DIRECTION_MINUS = -1 # 探索の際に属性や金額を増やす方向で動く
+sol_poplation = [] # 解の集合
+eval_poplation = [] # 解の評価値の集合 = 添え字番号を解の集合と合わせる
 
-sol_poplation = []
-eval_poplation = []
-
-def create_sol(create_num, function_id, city, seeds):
+def create():
 	'''
-	解の生成
-
-	Args:
-	- create_num (int, optional): . 生成する解の数 / Defaults : 0.
+	解集団の新規生成
 	'''
-	for i in range(create_num):
-		sol_item = create.create_init_sol(function_id, city, seeds)
+	for _ in range(const.POPLATION_MAX):
+		sol_item = creater.create_init_sol()
+		sol_poplation.append(sol_item)
 
 		log.debug('create-solution:' + str(sol_item))
 
-		sol_poplation.append(sol_item)
 	log.debug('======================')
 
-
-def evaluate_sol(ex_eval_func):
+def evaluate(ex_eval_func):
 	'''
 	呼び出し元から受け取った評価関数をもとに、各解を評価
 
@@ -48,9 +41,7 @@ def evaluate_sol(ex_eval_func):
 	eval_poplation = ex_eval_func(sol_poplation)
 	graph.update_create_optimal_solution_data_file(eval_poplation)
 
-
-
-def evolve_sol():
+def evolve():
 	'''
 	解を進化させる
 	'''
@@ -77,12 +68,7 @@ def evolve_sol():
 			if i % 2 == 0:
 				sol_poplation[i] = evolution.challenge_evolve_prudent(sol_poplation[i],eval_poplation[i]['feasible'])
 			else:
-			 	sol_poplation[i] = evolution.challenge_evolve_agressive(sol_poplation[i])
-
-			
-
+				sol_poplation[i] = evolution.challenge_evolve_agressive(sol_poplation[i])
 	log.info('evolve after:' + str(sol_poplation))
-
-	pass
 	
 
