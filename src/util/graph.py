120019de2ecf89eec9f22b83ea0d1cf92df28ae3
SoraY677 <SoraY677@outlook.com>
2021-12-05 16:16:38 +0900

    コメントを追加

--- src/util/graph.py
+++ src/util/graph.py
@@ -6,6 +6,12 @@ optimal_solution_list = []
 max_optimal = 0
 
 def _add_optimal_solution2list(res_list):
+	'''
+	評価値をリストに追加
+
+	Args:
+		res_list (list): 1世代における評価値のリスト
+	'''
 	global optimal_solution_list
 	global max_optimal
 	for i in range(len(res_list)):
@@ -13,10 +19,20 @@ def _add_optimal_solution2list(res_list):
 	optimal_solution_list.append(max_optimal)
 
 def _create_optimal_solution_data_file():
+	'''
+	評価値をデータファイルに記録
+	'''
 	global optimal_solution_list
 	with open(path.join(const.ANALYZE_OPTIMAZE_SOLUTION, 'data.json'), 'w') as f:
 		f.write(str(optimal_solution_list))
 
 def update_create_optimal_solution_data_file(res_list):
+	'''
+	評価値を更新
+	世代交代時に実行される想定
+
+	Args:
+		res_list (list): 1世代における評価値のリスト
+	'''
 	_add_optimal_solution2list(res_list)
 	_create_optimal_solution_data_file()
