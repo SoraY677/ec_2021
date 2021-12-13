17cd9306fc81845c3b2e4b812f753390a548ec98
SoraY677 <SoraY677@outlook.com>
2021-12-13 20:08:11 +0900

    時間計測の記録をログへ

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
+
+from time import time
 
 import config
 from src import process
@@ -8,7 +9,7 @@ from src.util import log
 if __name__ == "__main__":
 
 	# タイマー計測開始
-	start_time = time.time()
+	start_time = time()
 
 	# function_id の要素数を取得
 	const.FUNCTION_ID_LEN = len(eval(const.FUNCTION_ID))
@@ -17,17 +18,19 @@ if __name__ == "__main__":
 	process.create()
 	loop_num = const.SEARCH_MAX / const.POPLATION_MAX
 
-	for _ in range(int(loop_num)):
+	log.info('処理の生成終了時の処理時間:'  + str(time() - start_time))
+
+	for i in range(int(loop_num)):
 		# 解の評価
 		process.evaluate()
-
 		# 進化
 		process.evolove()
 
-	# タイマー計測終了
-	end_time = time.time()
+		log.info('第'+ str(i+1) +'進化:'  + str(time() - start_time))
 
-	log.info('経過時間:' + str(end_time - start_time) )
+	# タイマー計測終了
+	end_time = time()
+	log.info('総合処理時間:' + str(end_time - start_time) )
 
 
 
